a746e3fbc6621f319c74280c7dad4afee9abfb0f
De-Grompa <jeremywalz99@gmail.com>
2023-05-30 20:48:54 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'CS575-Project2-Phase1';
+}
